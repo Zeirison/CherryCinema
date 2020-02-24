9b2b89380446eb1f6aee24738c291dcb42e266c6
Dmitry <dmitry.soldatenko@agilie.com>
2020-02-24 17:42:06 +0200

    Add images link and api key for testing

--- app/src/main/java/com/example/cherrycinema/utils/Constants.kt
+++ app/src/main/java/com/example/cherrycinema/utils/Constants.kt
@@ -1,4 +1,5 @@
 package com.example.cherrycinema.utils
 
 const val API_URL: String = "https://api.themoviedb.org/3/"
-const val API_KEY: String = ""
+const val API_IMAGES_URL: String = "https://image.tmdb.org/t/p/w500/"
+const val API_KEY: String = "064de7c7aa371de8e3922d66630342b5"
